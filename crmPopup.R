ffba0feea7375945fee067cc2d1ef16c035d680c
Jathan <Aishwarya.Jathan@nrc-cnrc.gc.ca>
2026-09-04 16:45:03 -0300

    Fixed popup analyte table bug

--- crmPopup.R
+++ crmPopup.R
@@ -27,6 +27,9 @@ output$js_code2 <- renderUI({
   ))
 })
 
+# define analyteTable as reactiveVal so that it can change between null and a dataframe
+analyteTable <- reactiveVal(NULL)
+
 #waits for a click to a crm and displays the corresponding popup
 observeEvent(input$clicked_name, {
   name <- input$clicked_name
@@ -47,9 +50,11 @@ observeEvent(input$clicked_name, {
   ddf = rvest::html_table(html_nodes(read_html(geturl(link, h)),'table'))
   rm(h)
   #sets analyte table data to null, unless crm contains analyte table
-  analyteTable <- NULL
+
   if(length(ddf) >= 3 & grepl('Analyte',paste(ddf[3]))){
-    analyteTable <- data.frame(ddf[[3]])
+    analyteTable(data.frame(ddf[[3]]))
+  } else {
+    analyteTable(NULL)
   }
   
   table <- as.data.frame(ddf[[1]])
@@ -57,8 +62,15 @@ observeEvent(input$clicked_name, {
   pubDate <- table[table$X1 == "Publication date", "X2"]
   doi <- table[table$X1 == "DOI", "X2"]
   
-  output$popupanalyteTable <- renderDT({
-    datatable(analyteTable, options = list(pageLength = 10, responsive = FALSE, scrollX = TRUE))
+
+  output$popupanalyteTableUI <- renderUI({
+    if (!is.null(analyteTable())) {
+      DTOutput("popupanalyteTable")
+    }
+  })
+  
+  output$popupanalyteTable <-renderDT({
+    datatable(analyteTable(), options = list(pageLength = 10, responsive = FALSE, scrollX = TRUE))
   })
   
   output$downloadAnalyteTable <- downloadHandler(
@@ -66,12 +78,12 @@ observeEvent(input$clicked_name, {
       paste("Analyte Table.csv")
     },
     content = function(file) {
-      write.csv(analyteTable, file, row.names = FALSE)
+      write.csv(analyteTable(), file, row.names = FALSE)
     }
   )
   
   #only render the download button if the analyte table exists
-  if(is.null(analyteTable)) {
+  if(is.null(analyteTable())) {
     output$showDownloadButton <- renderUI({ })
   } else {
     output$showDownloadButton <- renderUI({
@@ -86,7 +98,7 @@ observeEvent(input$clicked_name, {
     p(abstract),
     a(paste("DOI:", gsub("Resolve DOI: ", "", doi)), href=gsub("Resolve DOI: ", "", doi), target="_blank"),
     p(pubDate),
-    DTOutput("popupanalyteTable"),
+    uiOutput("popupanalyteTableUI"),
     uiOutput("showDownloadButton"),
     easyClose = TRUE,
     footer = modalButton("Close")
